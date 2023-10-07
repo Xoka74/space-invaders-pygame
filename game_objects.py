11fdb50bbd715fb0eace3f1e59114a66fdd8ac4e
Xoka74 <ya.vladimir565@mail.ru>
2023-10-08 02:46:57 +0500

    Reworked game objects to be suitable with sprite Groups

--- game_objects.py
+++ game_objects.py
@@ -1,100 +1,86 @@
 import random
 from copy import copy
-from dataclasses import dataclass, field
 
 import pygame
 from pygame import Surface, time
 from pygame.math import Vector2
 import time
+
+from pygame.sprite import Group
+
 import config
 from repositories import images
 
 
-@dataclass(unsafe_hash=True)
 class GameObject(pygame.sprite.Sprite):
-    sprite: Surface
-    pos: Vector2
-    vel: Vector2
-
-    def __post_init__(self):
+    def __init__(self, image: Surface, pos: Vector2, vel: Vector2):
         super().__init__()
-
-    @property
-    def rect(self):
-        return self.sprite.get_rect(center=self.pos)
+        self.image = image
+        self.pos = pos
+        self.vel = vel
+        self.rect = self.image.get_rect(center=self.pos)
 
     def update(self):
         self.pos += self.vel
         self.rect.center = self.pos
 
-    def get_distance(self, other):
-        return self.pos.distance_to(other.pos) <= 50
 
-
-@dataclass
 class Invader(GameObject):
-    points: int
+    def __init__(self, image, pos, vel, points: int):
+        super().__init__(image, pos, vel)
+        self.points = points
 
 
-@dataclass
 class Bullet(GameObject):
-    shooter: GameObject
-
-    def __post_init__(self):
+    def __init__(self, pos, vel, shooter: GameObject):
+        super().__init__(images.get('bullet'), pos, vel)
+        self.shooter = shooter
         self.pos = copy(self.shooter.pos)
 
 
-@dataclass
 class Player(GameObject):
-    health: int
-    reload_seconds: int
-    last_reload_time: float = time.process_time()
-    reloading: bool = False
-
-    def __post_init__(self):
-        self.last_reload_time = time.process_time() - self.reload_seconds
+    def __init__(self, pos, vel, health: int, reload_seconds: int):
+        super().__init__(images.get('player'), pos, vel)
+        self.health = health
+        self.reload_seconds = reload_seconds
+        self.last_reload_time = time.process_time()
+        self.reloading = False
 
     def update(self):
         super().update()
 
         self.reloading = time.process_time() - self.last_reload_time <= self.reload_seconds
 
-        if self.pos.x <= 0:
-            self.pos.x = 0
-        elif self.pos.x >= config.SCREEN_WIDTH:
-            self.pos.x = config.SCREEN_WIDTH
+        if self.pos.x <= self.image.get_width() / 2:
+            self.pos.x = self.image.get_width() / 2
+        elif self.pos.x >= config.SCREEN_WIDTH - self.image.get_width() / 2:
+            self.pos.x = config.SCREEN_WIDTH - self.image.get_width() / 2
 
     def reload(self):
         self.last_reload_time = time.process_time()
 
 
-@dataclass
 class Bunker(GameObject):
-    state: int
+    def __init__(self, pos, vel, state: int):
+        super().__init__(images.get('bunker_default'), pos, vel)
+        self.state = state
 
     def update(self):
-        self.sprite = images.get(f'bunker_{self.state}')
+        self.image = images.get(f'bunker_{self.state}')
 
 
-class OtherBunker(pygame.sprite.Sprite):
-    def __init__(self, image: Surface, pos: Vector2, vel: Vector2):
-        super().__init__()
-        self.image = image
-        self.rect = self.image.get_rect(center=pos)
-        self.pos = pos
-        self.vel = vel
-
-
-@dataclass
 class MysteryShip(GameObject):
-    duration: float
-    points: float
-    started_time: float = time.process_time()
-    started: bool = False
+    def __init__(self, pos, vel, duration, points):
+        super().__init__(images.get('mystery_ship'), pos, vel)
+        self.duration = duration
+        self.points = points
+        self.started_time = time.process_time()
+        self.started = False
 
     def update(self):
         super().update()
-        self.started = (time.process_time() - self.started_time) <= self.duration
+        if (time.process_time() - self.started_time) >= self.duration:
+            self.end()
 
         if self.pos.x <= 50:
             self.pos.x = 50
@@ -103,22 +89,23 @@ class MysteryShip(GameObject):
             self.pos.x = config.SCREEN_WIDTH - 50
             self.vel *= -1
 
+    def start(self):
+        self.pos = Vector2(x=random.randint(0, config.SCREEN_WIDTH), y=25)
+        self.started = True
+        self.started_time = time.process_time()
 
-@dataclass
+    def end(self):
+        self.started = False
+        self.kill()
+
+
 class Level:
-    name: str
-    invaders: list[Invader]
-    bunkers: list[Bunker]
-    player: Player
-    bullets: list[Bullet] = field(default_factory=lambda: [])
-    mystery_ship: MysteryShip = None
-    score_val: int = 0
-
-    def start_mystery_ship(self):
-        random_y_cord = random.randint(0, config.SCREEN_WIDTH)
-        self.mystery_ship = MysteryShip(images.get('mystery_ship'), Vector2(x=25, y=25), Vector2(1, 0),
-                                        duration=5,
-                                        points=100)
-
-    def end_mystery_ship(self):
-        self.mystery_ship = None
+    def __init__(self, name: str, invaders: Group, bunkers: Group, player: Player, mystery_ship: MysteryShip):
+        self.name = name
+        self.invaders = invaders
+        self.bunkers = bunkers
+        self.player = player
+        self.mystery_ship = mystery_ship
+        self.bullets = pygame.sprite.Group()
+        self.score_val: int = 0
+        self.high_score: int = 0
